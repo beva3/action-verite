8b472dc124a0f281c23d0f30c14548c1a0dcdbbd
beva3 <mizabienvenu@gmail.com>
2024-11-25 22:23:31 +0530

    run game backend

--- Truth_dare.js
+++ Truth_dare.js
@@ -3,22 +3,47 @@ class Gamme_truth_dare{
         this.truth = []
         this.dare  = []
         this.choices = {
-            truth: "Truth",
-            dare: "Dare"
+            1: "Truth",
+            2: "Dare"
         }
     }
 
     choice(){
-        return this.choices['truth']
+        let index = Math.floor(Math.random() * 2 +1)
+        console.log(index);
+        
+        return this.choices[index]
     }
     generate(game){
-        console.log(`playing ${game} game`);
+        let index = Math.floor(Math.random() * game.length)
+        console.log(`${game[index]}`);
+        
     }
     play(){
-        console.log("Playing");
-        let game = this.choice();
-        this.generate(game);
+        const truth = [
+            "Tell the truth",
+            "Be honest",
+            "Share your thoughts and feelings",
+            "Be true to yourself"
+        ]
+
+        const dare = [
+            "Dare to lie",
+            "Be difficult",
+            "Do something you don't want to do",
+            "Do something out of your comfort zone"
+        ]
+        console.log(truth);
+        
+        game = this.choice()
+        console.log(`${game} game`);
+        if (game == "Truth") {
+            this.generate(truth)
+        }else if (game == "Dare"){
+            this.generate(dare)
+        }
     }
+    
 }
 
 let game = new Gamme_truth_dare();
